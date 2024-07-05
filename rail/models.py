a9149714500d90bdfea8a323dbe381a656f04a8b
Anna <annalepiska@gmail.com>
2024-07-05 16:57:35 +0300

    create Crew, Station, Route, TrainType, Train, Journey, Order, Ticket, User models & make migrations & add User as AUTH_USER_MODEL in settings

--- rail/models.py
+++ rail/models.py
@@ -1,3 +1,124 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+        verbose_name_plural = "crew_members"
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ("name", "latitude", "longitude")
+        ordering = ["name"]
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="routes_as_source"
+    )
+    destination = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="routes_as_destination"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source.name}-{self.destination.name}"
+
+    class Meta:
+        unique_together = ("source", "destination")
+        ordering = ["source__name", "destination__name"]
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        TrainType, on_delete=models.CASCADE, related_name="trains"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["train_type", "name"]
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="journeys"
+    )
+    train = models.ForeignKey(
+        Train, on_delete=models.CASCADE, related_name="journeys"
+    )
+    crew = models.ManyToManyField(Crew, related_name="journeys")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return (
+            f"{str(self.train)}, {str(self.route)}, "
+            f"departure: {self.departure_time}, arrival: {self.arrival_time}"
+        )
+
+    class Meta:
+        unique_together = ("route", "train", "departure_time")
+        ordering = ["-departure_time"]
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    carriage = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(
+        Journey, on_delete=models.CASCADE, related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="tickets"
+    )
+
+    def __str__(self):
+        return (
+            f"{str(self.journey)} (carriage: {self.carriage}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("carriage", "seat", "journey")
+        ordering = ["carriage", "seat"]
