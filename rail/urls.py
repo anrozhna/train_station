abb0eb8d7527b062b0923d8c5c8694221388cb54
Anna <annalepiska@gmail.com>
2024-07-08 16:30:29 +0300

    create base view and url for each model

--- rail/urls.py
+++ rail/urls.py
@@ -1,1 +1,28 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from rail.views import (
+    CrewViewSet,
+    StationViewSet,
+    RouteViewSet,
+    TrainTypeViewSet,
+    TrainViewSet,
+    JourneyViewSet,
+    OrderViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("crews", CrewViewSet)
+router.register("stations", StationViewSet)
+router.register("routes", RouteViewSet)
+router.register("train-types", TrainTypeViewSet)
+router.register("trains", TrainViewSet)
+router.register("journeys", JourneyViewSet)
+router.register("orders", OrderViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
 app_name = "rail"
--- rail/views.py
+++ rail/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from rail.models import (
+    Crew,
+    Station,
+    Route,
+    TrainType,
+    Train,
+    Journey,
+    Order,
+    Ticket,
+)
+from rail.serializers import (
+    CrewSerializer,
+    StationSerializer,
+    RouteSerializer,
+    TrainTypeSerializer,
+    TrainSerializer,
+    JourneySerializer,
+    OrderSerializer,
+    TicketSerializer,
+)
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all().prefetch_related("journeys")
+    serializer_class = CrewSerializer
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all().prefetch_related("source", "destination")
+    serializer_class = RouteSerializer
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all().prefetch_related("train_type")
+    serializer_class = TrainSerializer
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all().prefetch_related("route", "train", "crew")
+    serializer_class = JourneySerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
