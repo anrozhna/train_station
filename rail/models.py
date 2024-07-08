4e1f55f3b3a8f205af50adda5aa131bc91ab4a81
Anna <annalepiska@gmail.com>
2024-07-08 17:48:08 +0300

    add save method to the Ticket model

--- rail/models.py
+++ rail/models.py
@@ -156,3 +156,18 @@ class Ticket(models.Model):
             self.journey.train,
             ValidationError,
         )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert,
+            force_update,
+            using,
+            update_fields
+        )
