472f7caf98ae573a829a9ce1e4f443e276a1ae0f
Anna <annalepiska@gmail.com>
2024-07-08 13:53:08 +0300

    create TicketInline & OrderAdmin

--- rail/admin.py
+++ rail/admin.py
@@ -11,11 +11,21 @@ from rail.models import (
     Ticket,
 )
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [TicketInline]
+
+
 admin.site.register(Crew)
 admin.site.register(Station)
 admin.site.register(Route)
 admin.site.register(TrainType)
 admin.site.register(Train)
 admin.site.register(Journey)
-admin.site.register(Order)
 admin.site.register(Ticket)
