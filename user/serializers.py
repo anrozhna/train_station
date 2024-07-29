a9d50ee0d85fbe5ec9495d280922ffa1003d80ed
Anna <annalepiska@gmail.com>
2024-07-29 23:50:25 +0300

    create ManageUserView & update authorization through email

--- user/serializers.py
+++ user/serializers.py
@@ -7,9 +7,27 @@ from user.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5,
+                "style": {"input_type": "password"}
+            }
+        }
 
     def create(self, validated_data):
+        """Create User with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update User with encrypted password."""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
