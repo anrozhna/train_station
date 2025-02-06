0c361f02161475d20248eba9dd91210249dd4251
Anna <annalepiska@gmail.com>
2025-02-06 13:28:10 +0200

    update rail url

--- train_station/urls.py
+++ train_station/urls.py
@@ -27,7 +27,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rail/", include("rail.urls", namespace="rail")),
+    path("", include("rail.urls", namespace="rail")),
     path("user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
